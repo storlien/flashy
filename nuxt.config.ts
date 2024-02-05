9403726d1e37e198d1f6d9ea3bcdebc4abf88691
Nexerate <erl1ls@outlook.com>
2024-02-05 18:11:14 +0100

    Theme

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,7 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss', 'shadcn-nuxt'],
+  css: ['~/assets/css/main.css', '~/assets/css/tailwind.css'],
   shadcn: {
     /**
      * Prefix for all the imported component
--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,33 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const clickCount = ref(0);
+
+function onClick() {
+    clickCount.value++;
+}
+</script>
+
 <template>
     <div>
-        <p>Velkommen til Flashy!</p>
+        <!-- <p>Velkommen til Flashy!</p> -->
+        <FlashyButton @click="onClick">
+            <!-- <p>Denne knappen har blitt klikket {{ clickCount }} ganger</p> -->
+        </FlashyButton>
     </div>
 </template>
 
 <style lang="scss">
-p {
+button {
     position: absolute;
     left: 50%;
-    top: 50%;
-
-    font-weight: bold;
+    top: 60%;
 
     transform: translate(-50%, -50%);
+
+    // border: 2px solid gray;
+    // border-radius: 5px;
+    // padding: 5px 15px;
+
 }
 </style>
