6e259a6a923e8e57bed48d3dea6c8f5183b96154
Nexerate <erl1ls@outlook.com>
2024-02-05 14:03:09 +0100

    Default page to build upon

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <p>Velkommen til Flashy!</p>
+    </div>
+</template>
+
+<style lang="scss">
+p {
+    position: absolute;
+    left: 50%;
+    top: 50%;
+
+    font-weight: bold;
+
+    transform: translate(-50%, -50%);
+}
+</style>
