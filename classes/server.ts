2e65b63942eda960f1f96fbc090a9361e23c6960
Nexerate <erl1ls@outlook.com>
2024-02-20 14:15:41 +0100

    Function for creating a flashcard set in firebase

--- classes/server.ts
+++ classes/server.ts
@@ -2,7 +2,7 @@ import type { FirebaseApp } from 'firebase/app';
 import { Authenticator } from './authenticator';
 
 import { initializeApp, type FirebaseOptions } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
+import { addDoc, collection, getFirestore } from 'firebase/firestore';
 
 const config: FirebaseOptions = {
     apiKey: 'AIzaSyDFEhtsAA0IkzSD6PRJHFw6zfAdFee2OZc',
@@ -19,6 +19,32 @@ class Server {
 
     constructor(app: FirebaseApp) {
         this.auth = new Authenticator(app);
+    }
+
+    public async createFlashcardSet(name: string, category: string, flashcards: Flashcard[]): Promise<FlashcardSet | null> {
+        if (!this.auth.isLoggedIn()) throw new Error('Unauthorized');
+
+        const set = {
+            name,
+            category,
+            flashcards,
+        };
+
+        try {
+            const docRef = await addDoc(collection(db, 'flashcard-sets'), set);
+            return docRef as unknown as FlashcardSet;
+        } catch (error) {
+            Server.logError(error);
+            return null;
+        }
+    }
+
+    /** Log an error caught in a try-catch. */
+    private static logError(error: any) {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+
+        console.error(errorCode, errorMessage);
     }
 }
 
