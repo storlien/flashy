82595072780d3a156036329ce18c871d10b47283
Elias Strømsnes <eliasgst@stud.ntnu.no>
2024-03-01 12:39:08 +0100

    Discovery page with search function

---
+++ classes/discovery-columns.ts
@@ -0,0 +1,47 @@
+import { h } from 'vue';
+import type { ColumnDef } from '@tanstack/vue-table';
+import DropdownAction from '@/components/DataTableDropDown.vue';
+import type { Flashcard, FlashcardSet } from './models';
+
+const columns: ColumnDef<FlashcardSet>[] = [
+    {
+        accessorKey: 'name',
+        header: () => h('div', { class: 'text-right' }, 'Navn'),
+        cell: ({ row }) => {
+            const name = row.getValue('name') as unknown as string;
+
+            return h('div', { class: 'text-right font-medium' }, name);
+        },
+    },
+    {
+        accessorKey: 'creator',
+        header: () => h('div', { class: 'text-right' }, 'Skaper'),
+        cell: ({ row }) => {
+            const creator = row.getValue('creator') as unknown as string;
+
+            return h('div', { class: 'text-right font-medium' }, creator);
+        },
+    },
+    {
+        accessorKey: 'category',
+        header: () => h('div', { class: 'text-right' }, 'Kategori'),
+        cell: ({ row }) => {
+            const category = row.getValue('category') as unknown as string;
+
+            return h('div', { class: 'text-right font-medium' }, category);
+        },
+    },
+    {
+        accessorKey: 'flashcards',
+        header: () => h('div', { class: 'text-right' }, 'Antall Kort'),
+        cell: ({ row }) => {
+            const flashcards = row.getValue('flashcards') as Flashcard[];
+
+            return h('div', { class: 'text-right font-medium' }, flashcards.length);
+        },
+    },
+]
+
+export {
+    columns
+}
