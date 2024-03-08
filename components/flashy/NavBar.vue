ec83d52a8a2e91493e9f546719af5e43a2cb972e
hannajacobsen <hannajac@stud.ntnu.no>
2024-03-08 11:37:49 +0100

    created aboutFlashy.vue page. The navigation in the navbar is now working, but is not finised yet - missing last touches such as colors and details.

--- components/flashy/NavBar.vue
+++ components/flashy/NavBar.vue
@@ -25,13 +25,13 @@ import {
     <div class="hidden w-full md:block md:w-auto" id="navbar-default">
       <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
         <li>
-          <a href="#" class="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">Om Flashy</a>
+          <RouterLink :class="{'text-white bg-blue-700': $route.path === '/aboutFlashy', 'text-gray-900 hover:bg-gray-100': $route.path !== '/aboutFlashy'}" to="/aboutFlashy" aria-current="page">Om Flashy</RouterLink>
         </li>
         <li>
-          <a href="#" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Profil</a>
+          <RouterLink :class="{'text-white bg-blue-700': $route.path === '/profile', 'text-gray-900 hover:bg-gray-100': $route.path !== '/profile'}" to="/profile">Profil</RouterLink>
         </li>
         <li>
-          <a href="#" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Utforsk</a>
+          <RouterLink :class="{'text-white bg-blue-700': $route.path === '/explore', 'text-gray-900 hover:bg-gray-100': $route.path !== '/explore'}" to="/explore">Utforsk</RouterLink>
         </li>
       </ul>
     </div>
@@ -66,6 +66,7 @@ import {
 
 .logo img {
   display: flex;
+  justify-content: center;
   width: 120px; 
   height: auto; 
   margin-left: 120px;
---
+++ pages/aboutFlashy.vue
@@ -0,0 +1,73 @@
+<template>
+    <div id="profile">
+      <div id="center-column">
+        <div id="navBar">
+          <NavBar />
+        </div>
+        <div class="container">
+          <h1 class="header">Flashy</h1>
+          <div class="about">
+            Flashy er en interaktiv flashcard-app laget for å forbedre læring og minne. 
+            Brukere kan enkelt opprette, administrere og interagere med et bredt spekter av læringssett. 
+            Appen tilbyr både private og offentlige sett, samt funksjoner som gjør det mulig å merke vanskelige kort, 
+            legge til bilder og engasjere seg med andre brukeres sett gjennom likes og kommentarer. 
+            Med kraftige søke- og filtreringsfunksjoner, samt muligheten til å lagre favorittsett, 
+            tilbyr Flashy en tilpassbar læringsopplevelse for enhver bruker. Med et intuitivt grensesnitt og 
+            verktøy for profiladministrasjon, lagring og organisering, interaksjon med flashcards, 
+            bildeintegrasjon, moderering og oversikt over populære sett, er Flashy det ideelle verktøyet for 
+            alle læringssituasjoner.
+            **Sikkert lurt å endre litt på denne teksten**
+          </div>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script setup lang="ts">
+  import NavBar from '@/components/flashy/NavBar.vue';
+  
+  definePageMeta({
+    middleware: 'auth',
+  });
+  </script>
+  
+  <style scoped>
+  #profile {
+    display: grid;
+    position: relative;
+    width: 100vw;
+    height: 100vh;
+    grid-template-columns: 1fr;
+    border-radius: 10px; 
+    overflow: hidden; 
+  }
+  
+  #center-column {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
+  }
+  
+  .container {
+    max-width: 800px;
+    margin: auto;
+    text-align: center;
+  }
+  
+  .header {
+    font-size: 40px;
+    font-weight: bold;
+    color: #cf1f3c;
+    margin-bottom: 20px;
+  }
+  
+  .about {
+    font-size: 18px;
+    text-align: center;
+    margin-left: 50px;
+    margin-right: 50px;
+  }
+  </style>
+  
