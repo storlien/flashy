9d5de7e7feb67b5ca090ed640b17b23c13453db0
Kacper Kreczko <kacperjk@stud.ntnu.no>
2024-03-12 12:46:56 +0100

    completely changed the component to include an avatar image and be insertable to navbar #42

--- components/flashy/ManageProfile.vue
+++ components/flashy/ManageProfile.vue
@@ -1,33 +1,238 @@
 <template>
+  <div class="flex items-center justify-center">
+    <!-- add username display-->
+    <h3 class="mx-5 text-sm">Bruker</h3>
     <Dialog>
-        <DialogTrigger as-child>
-            <Button id="test" variant="outline">Rediger profil</Button>
-        </DialogTrigger>
-        <DialogContent class="sm:max-w-[425px]">
-            <DialogHeader>
-                <DialogTitle>Rediger profil</DialogTitle>
-                <DialogDescription>Gjør endringer til profilen din her. Klikk 'Lagre' når du er ferdig</DialogDescription>
-            </DialogHeader>
-            <div class="grid gap-4 py-4">
-                <!-- <div class="grid grid-cols-4 items-center gap-4">
-                    <Label for="username" class="text-right">Brukernavn</Label>
-                    <Input id="username" class="col-span-3" />
-                </div> -->
-                <div class="grid grid-cols-4 items-center gap-4">
-                    <Label for="email" class="text-right">E-post</Label>
-                    <Input id="email" class="col-span-3" />
+      <DialogTrigger as-child>
+        <Avatar class="cursor-pointer h-[45px] w-[45px]">
+          <AvatarImage src="" alt="User avatar" />
+          <AvatarFallback>
+            <span>{{
+              userSettings?.name ? userSettings.name.charAt(0) : "Bruker"
+            }}</span>
+          </AvatarFallback>
+        </Avatar>
+      </DialogTrigger>
+      <DialogContent class="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Rediger profil</DialogTitle>
+        </DialogHeader>
+
+        <form @submit.prevent="onSubmit" class="grid gap-4 py-4">
+            <div class="relative flex justify-center align-center pb-5">
+              <Label for="picture">
+                <Avatar class="h-[160px] w-[160px] cursor-pointer">
+                  <AvatarImage src="" alt="User avatar" class="w-full h-full" />
+                  <AvatarFallback class="text-xl">
+                    <span>{{
+                      userSettings?.name
+                        ? userSettings.name.charAt(0)
+                        : "Bruker"
+                    }}</span>
+                  </AvatarFallback>
+                </Avatar>
+                <div
+                  class="absolute right-[115px] bottom-[25px] z-10 rounded-full overflow-hidden bg-[#dd1d4a] h-[35px]"
+                >
+                  <Pencil
+                    color="white"
+                    class="w-[35px] h-[35px] p-[10px] cursor-pointer"
+                  ></Pencil>
                 </div>
-                <div class="grid grid-cols-4 items-center gap-4">
-                    <Label for="password" class="text-right">Passord</Label>
-                    <Input id="password" class="col-span-3" />
-                </div>
+              </Label>
+              <!--Needs vmodel and stuff -->
+              <Input id="picture" type="file" style="display: none" />
             </div>
+            <!--<FormField name="avatar">
+              <Label for="picture">
+                <Avatar class="h-[160px] w-[160px] cursor-pointer">
+                  <AvatarImage src="" alt="User avatar" class="w-full h-full" />
+                  <AvatarFallback class="text-xl">
+                    <span>{{
+                      userSettings?.name
+                        ? userSettings.name.charAt(0)
+                        : "Bruker"
+                    }}</span>
+                  </AvatarFallback>
+                  <Input type="file" style="display: none" />
+                </Avatar>
+                <div
+                  class="absolute right-[115px] bottom-[25px] z-10 rounded-full overflow-hidden bg-[#dd1d4a] h-[35px]"
+                >
+                  <Pencil
+                    color="white"
+                    class="w-[35px] h-[35px] p-[10px] cursor-pointer"
+                  ></Pencil>
+                </div>
+              </Label>
+              
+            </FormField>
+          -->
+
+            <FormField v-slot="{ componentField }" name="username">
+              <FormControl>
+                <div class="grid grid-cols-4 items-center gap-4">
+                  <Label for="username" class="text-right">Brukernavn</Label>
+                  <Input
+                    class="col-span-3"
+                    type="text"
+                    id="username"
+                    v-bind="componentField"
+                    v-model="username"
+                  />
+                </div>
+              </FormControl>
+            </FormField>
+
+            <FormField v-slot="{ componentField }" name="email">
+              <FormItem v-auto-animate>
+                <FormControl>
+                  <div class="grid grid-cols-4 items-center gap-4">
+                    <Label for="email" class="text-right">E-post</Label>
+                    <Input
+                      type="email"
+                      id="email"
+                      class="col-span-3"
+                      v-bind="componentField"
+                      v-model="email"
+                    />
+                  </div>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            </FormField>
+            <FormField v-slot="{ componentField }" name="password">
+              <FormItem v-auto-animate>
+                <FormControl>
+                  <div class="grid grid-cols-4 items-center gap-4">
+                    <Label for="password" class="text-right">Nytt Passord</Label>
+                    <Input
+                      type="password"
+                      id="password"
+                      class="col-span-3"
+                      v-bind="componentField"
+                      placeholder="Skriv her"
+                    />
+                  </div>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            </FormField>
             <DialogFooter>
-                <Button type="submit">Lagre</Button>
+              <Button id="logout-button" class="absolute left-6">
+                <NuxtLink to="/login">Logg ut</NuxtLink>
+              </Button>
+              <Button type="submit">Lagre</Button>
             </DialogFooter>
-        </DialogContent>
+        </form>
+
+        <!--<div class="grid gap-4 py-4">
+          <div class="relative flex justify-center align-center pb-5">
+            <Label for="picture">
+              <Avatar class="h-[160px] w-[160px] cursor-pointer">
+                <AvatarImage src="" alt="User avatar" class="w-full h-full" />
+                <AvatarFallback class="text-xl">
+                  <span>{{
+                    userSettings?.name ? userSettings.name.charAt(0) : "Bruker"
+                  }}</span>
+                </AvatarFallback>
+              </Avatar>
+              <div
+                class="absolute right-[115px] bottom-[25px] z-10 rounded-full overflow-hidden bg-[#dd1d4a] h-[35px]"
+              >
+                <Pencil
+                  color="white"
+                  class="w-[35px] h-[35px] p-[10px] cursor-pointer"
+                ></Pencil>
+              </div>
+            </Label>
+            Needs vmodel and stuff
+            <Input id="picture" type="file" style="display: none" />
+          </div>
+          <div class="grid grid-cols-4 items-center gap-4">
+            <Label for="username" class="text-right">Brukernavn</Label>
+            <Input id="username" class="col-span-3" v-model="username" />
+          </div>
+          <div class="grid grid-cols-4 items-center gap-4">
+            <Label for="email" class="text-right">E-post</Label>
+            <Input type="email" id="email" class="col-span-3" v-model="email" />
+          </div>
+          <div class="grid grid-cols-4 items-center gap-4">
+            <Label for="password" class="text-right">Passord</Label>
+            <Input
+              disabled
+              type="password"
+              id="password"
+              class="col-span-3"
+              v-model="password"
+            />
+          </div>
+        </div>
+        <DialogFooter>
+          <Button id="logout-button" class="absolute left-6">
+            <NuxtLink to="/login">Logg ut</NuxtLink>
+          </Button>
+          <Button type="submit">Lagre</Button>
+        </DialogFooter>
+      -->
+      </DialogContent>
     </Dialog>
+  </div>
 </template>
 
-<style lang="ts">
-</style>
+<style lang="scss" scoped></style>
+
+<script setup lang="ts">
+import type { UserSettings } from "~/classes/models";
+import { Pencil } from "lucide-vue-next";
+import { vAutoAnimate } from "@formkit/auto-animate/vue";
+import { toTypedSchema } from "@vee-validate/zod";
+import { useForm } from "vee-validate";
+import { useRouter } from "vue-router";
+import * as z from "zod";
+import server from "~/classes/server";
+
+const userSettings = ref<UserSettings | null>();
+
+const username = ref("User");
+const email = ref("Not found");
+const password = ref("********");
+
+onMounted(async () => {
+  userSettings.value = await server.getUserSettings();
+
+  if (!userSettings.value) {
+    userSettings.value = await server.createUserSettings();
+  }
+
+  username.value = userSettings.value?.name || "Bruker";
+  email.value = userSettings.value?.email || "Ikke funnet";
+});
+
+const router = useRouter();
+
+const formSchema = toTypedSchema(
+  z.object({
+    username: z.string().min(2, "Username must be at least 2 characters"),
+    email: z.string().email("Must be a valid email"),
+    password: z
+      .string()
+      .min(6, "Password must be at least 6 characters")
+      .max(25, "Password must be less than 25 characters")
+      .regex(/.*[0-9].*/, "Password must contain at least one number"),
+  })
+);
+
+const { handleSubmit } = useForm({
+  validationSchema: formSchema,
+});
+
+const onSubmit = handleSubmit(async (formData) => {
+  console.log(formData);
+  await server.updateUserName(formData.username);
+  await server.updateUserEmail(formData.email);
+  await server.updateUserPassword(formData.password);
+  console.log("Submit");
+  router.push({ name: "profile" });
+});
+</script>
