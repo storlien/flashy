719022c006c70d6a40dfb3e11bbb36380c76e552
Kacper Kreczko <kacperjk@stud.ntnu.no>
2024-03-12 12:47:19 +0100

    tweaked navbar for the new profile trigger #42

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,30 +1,30 @@
 <template>
   <nav class="flex flex-row justify-between align-middle relative">
-    <div class="flex flex-row align-middle justify-center">
-      <RouterLink to="/about" :class="{ 'selected': $route.path === '/about' }">
+    <img src="/images/flashyLogo.jpg" class="h-12 absolute left-4 bottom-2">
+    <div class="flex flex-row align-middle justify-center ml-32">
+      <RouterLink to="/about" :class="{ 'selected': $route.path === '/about' }" class="centered-text">
         Om Flashy
       </RouterLink>
-      <RouterLink to="/profile" :class="{ 'selected': $route.path === '/profile' }">
+      <RouterLink to="/profile" :class="{ 'selected': $route.path === '/profile' }" class="centered-text">
         Profil
       </RouterLink>
-      <RouterLink to="/discover" :class="{ 'selected': $route.path === '/discover' }">
+      <RouterLink to="/discover" :class="{ 'selected': $route.path === '/discover' }" class="centered-text">
         Utforsk
       </RouterLink>
     </div>
-    <img src="/images/flashyLogo.jpg" class="h-12 absolute right-4 bottom-2">
+    <ManageProfile></ManageProfile>
   </nav>
 </template>
 
 <style lang="scss">
 nav {
-  padding: 20px;
-
+  padding: 12px;
   border-bottom: 2px solid #f0f0f0;
   margin-bottom: -2px;
 
   .selected {
     font-weight: bold;
-    color: #cf1f3c;
+    color: #dd1d4a;
   }
 
   a {
@@ -34,5 +34,16 @@ nav {
   .nav-container {
     width: 100vw;
   }
+
+  .centered-text {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
 }
 </style>
+
+<script setup lang="ts">
+import ManageProfile from './flashy/ManageProfile.vue';
+
+</script>
